81b059fc8023b7793b843a51362ff57e67d4502b
julio1927 <38108470+julio1927@users.noreply.github.com>
2021-11-09 13:41:17 -0500

    initial client commit

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+// initiate react application
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.querySelector('#root'));
